0b86cf10b80f647daaadc37d222ae1a9c7e24178
dnanto <dnegron2@gmu.edu>
2022-04-27 15:59:45 -0400

    Update addlocus.py

--- workflow/scripts/py/addlocus.py
+++ workflow/scripts/py/addlocus.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 from Bio import SeqIO
 
 record = SeqIO.read(sys.argv[1], "genbank")
 for feature in record.features:
   if feature.type == "CDS":
-      feature.qualifiers["locus_tag"] = feature.qualifiers.get("gene", feature.qualifiers["product"])
+    feature.qualifiers["locus_tag"] = [
+      re.sub(
+        r"[^a-zA-Z0-9*_-]+",
+        "_",
+        feature.qualifiers.get("gene", feature.qualifiers["product"])[0]
+      )
+    ]
 
 SeqIO.write(record, sys.stdout, "genbank")
